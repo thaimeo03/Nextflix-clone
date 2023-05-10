e6e420809e36c5b26cd335533fbabf2305d52ebf
Thai <thaitran15072003@gmail.com>
2023-05-10 23:26:23 +0700

    Set up project and code UI auth

---
+++ app/auth/components/Input.tsx
@@ -0,0 +1,28 @@
+interface Props {
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+export default function Input({ id, onChange, value, label, type }: Props) {
+    return (
+        <div className="relative">
+            <input
+                onChange={onChange}
+                type={type}
+                value={value}
+                id={id}
+                className="block rounded-md px-6 pt-6 pb-1 w-full text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"
+                placeholder=" "
+            />
+            <label
+                htmlFor={id}
+                className="absolute text-zinc-400 duration-150 transform scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+            >
+                {label}
+            </label>
+        </div>
+    );
+}
